ae0ee17fe7a1cd6b8732d327ab92b77603a8b150
zlade30 <zpaquibot@medsoftgroup.com>
2020-07-19 13:11:40 +0800

    initialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './shared/scss/main.scss'
+import Me from './containers/landing-page/me'
+
+const App = () => {
+  return (
+    <div>
+      <Me />
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/landing-page/me.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd'
+
+const Me = () => {
+  return (
+    <div>
+      <Button>Hello World</Button>
+    </div>
+  )
+}
+
+export default Me
